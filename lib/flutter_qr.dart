70798a53148ab6ea6542f102b1bf8916fb76e0a9
tielan.fu <tielan.fu@chinacreator.com>
2019-02-22 10:30:44 +0800

    '添加页面示例'

--- lib/flutter_qr.dart
+++ lib/flutter_qr.dart
@@ -3,8 +3,8 @@ import 'package:flutter/services.dart';
 
 class FlutterQr {
   static const MethodChannel _channel = const MethodChannel('flutter_qr');
-  static Future<String> scan() async {
-    Map params = {'anim':false};//anim = false 跳转无动画 true Android 自带动画
+  static Future<String> scan({bool anim = false}) async {
+    Map params = {'anim':anim};//anim = false 跳转无动画 true Android 自带动画
     return await _channel.invokeMethod('readQRCode', params);
   }
 }
